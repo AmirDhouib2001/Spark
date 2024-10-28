b7be4052c5f5c74d94340128290a436e74a4fda0
NabilGarmouti <120674423+NabilGarmouti@users.noreply.github.com>
2024-10-28 14:56:25 +0100

    Update V2 "time and path"

--- src/fr/hymaia/exo4/python_udf.py
+++ src/fr/hymaia/exo4/python_udf.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
+import time
 
 # Initialisation de la Spark session
 spark = SparkSession.builder.appName("Python UDF").getOrCreate()
@@ -13,8 +14,16 @@ def get_category_name(category):
 category_name_udf = udf(get_category_name, StringType())
 
 # Chargement des données
-sell_df = spark.read.csv('resources/exo4/sell.csv', header=True, inferSchema=True)
+sell_df = spark.read.csv('src/resources/exo4/sell.csv', header=True, inferSchema=True)
+
+# Mesurer le temps d'exécution
+start_time = time.time()
 
 # Application de l’UDF pour ajouter la colonne `category_name`
 sell_df = sell_df.withColumn("category_name", category_name_udf(sell_df["category"]))
+
+end_time = time.time()
+print(f"Temps d'exécution du Python UDF : {end_time - start_time:.4f} secondes")
+
 sell_df.show()
+spark.stop()
