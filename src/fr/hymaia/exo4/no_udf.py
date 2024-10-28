eab80699742e92bc4751f6fc6ece8d77257ba859
NabilGarmouti <120674423+NabilGarmouti@users.noreply.github.com>
2024-10-28 14:55:30 +0100

    Update "time"

--- src/fr/hymaia/exo4/no_udf.py
+++ src/fr/hymaia/exo4/no_udf.py
@@ -1,13 +1,24 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import when
+import time
 
-# SparkSession
+# Initialisation de la Spark session
 spark = SparkSession.builder.appName("No UDF").getOrCreate()
 
 # Chargement des données
-sell_df = spark.read.csv('resources/exo4/sell.csv', header=True, inferSchema=True)
+sell_df = spark.read.csv('src/resources/exo4/sell.csv', header=True, inferSchema=True)
 
-# Utilisation de `when` et `otherwise` pour ajouter `category_name`
-sell_df = sell_df.withColumn("category_name",
-                             when(sell_df["category"] < 6, "food").otherwise("furniture"))
+# Mesurer le temps d'exécution
+start_time = time.time()
+
+# Application de la logique sans UDF
+sell_df = sell_df.withColumn(
+    "category_name",
+    when(sell_df["category"] < 6, "food").otherwise("furniture")
+)
+
+end_time = time.time()
+print(f"Temps d'exécution du No UDF : {end_time - start_time:.4f} secondes")
+
 sell_df.show()
+spark.stop()
