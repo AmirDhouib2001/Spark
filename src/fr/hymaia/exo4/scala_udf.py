01375f44599121b0a9d473e4061ffae782d19e52
NabilGarmouti <120674423+NabilGarmouti@users.noreply.github.com>
2024-10-28 14:59:59 +0100

    Need more updates

--- src/fr/hymaia/exo4/scala_udf.py
+++ src/fr/hymaia/exo4/scala_udf.py
@@ -1,36 +1,34 @@
-# import pyspark.sql.functions as f
-# from pyspark.sql import SparkSession
-
-
-# spark = SparkSession.builder.appName("exo4").master("local[*]").getOrCreate()
-
-# def main():
-#     print("Hello world!")
-
-#import pyspark.sql.functions as f
-#from pyspark.sql import SparkSession
-#spark = SparkSession.builder.appName("exo4").master("local[*]").getOrCreate()
-#def main():
-#    print("Hello world!")
-
 from pyspark.sql import SparkSession
 from pyspark.sql.column import Column, _to_java_column, _to_seq
+import time
 
-# SparkSession avec le JAR Scala
-spark = SparkSession.builder \
-    .appName("Scala UDF") \
-    .config('spark.jars', 'src/resources/exo4/udf.jar') \
-    .getOrCreate()
+def main():
+    # SparkSession avec le JAR Scala
+    spark = SparkSession.builder \
+        .appName("Scala UDF") \
+        .config('spark.jars', 'src/resources/exo4/udf.jar') \
+        .getOrCreate()
 
-# Fonction pour accéder à l’UDF Scala
-def addCategoryName(col):
-    sc = spark.sparkContext
-    add_category_name_udf = sc._jvm.fr.hymaia.sparkfordev.udf.Exo4.addCategoryNameCol()
-    return Column(add_category_name_udf.apply(_to_seq(sc, [col], _to_java_column)))
+    # Fonction pour accéder à l’UDF Scala
+    def addCategoryName(col):
+        sc = spark.sparkContext
+        add_category_name_udf = sc._jvm.fr.hymaia.sparkfordev.udf.Exo4.addCategoryNameCol()
+        return Column(add_category_name_udf.apply(_to_seq(sc, [col], _to_java_column)))
 
-# Chargement des données
-sell_df = spark.read.csv('resources/exo4/sell.csv', header=True, inferSchema=True)
+    # Chargement des données
+    sell_df = spark.read.csv('src/resources/exo4/sell.csv', header=True, inferSchema=True)
 
-# Application de l’UDF Scala pour ajouter la colonne `category_name`
-sell_df = sell_df.withColumn("category_name", addCategoryName(sell_df["category"]))
-sell_df.show()
+    # Mesurer le temps d'exécution
+    start_time = time.time()
+
+    # Application de l’UDF Scala pour ajouter la colonne `category_name`
+    sell_df = sell_df.withColumn("category_name", addCategoryName(sell_df["category"]))
+
+    end_time = time.time()
+    print(f"Temps d'exécution du Scala UDF : {end_time - start_time:.4f} secondes")
+
+    sell_df.show()
+    spark.stop()
+
+if __name__ == "__main__":
+    main()
